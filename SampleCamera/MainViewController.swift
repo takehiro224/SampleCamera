102220f424425e9b287152a0d7fd9a943d580240
takehiro224 <mogu.224@gmail.com>
2018-10-01 21:33:25 +0900

    action 定義

---
+++ SampleCamera/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SampleCamera
+//
+//  Created by 渡邊丈洋 on 2018/09/26.
+//  Copyright © 2018年 takehiro. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func onCameraButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func onSaveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func onEditButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
